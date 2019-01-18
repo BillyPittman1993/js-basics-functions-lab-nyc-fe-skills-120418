2abef5e1fdc76b19a3ada976f37cbdceafb31c90
BillyPittman1993 <billytrading93@gmail.com>
2019-01-18 01:13:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 // Code your solution in this file!
 
-function distanceFromHqInBlocks(distance) {
+function distanceFromHqInBlocks(blocks) {
   headquarters = 42 
-if (distance - headquarters === 1) {
+if (blocks - headquarters === 1) {
 return 1 
 }
-else if (distance - headquarters === 8) { 
+else if (blocks - headquarters === 8) { 
 return 8 
   } 
-  else if (headquarters - distance === 8) {
+  else if (headquarters - blocks === 8) {
   return 8 
   }
 }
 
+function distanceFromHqInFeet(feet) {
+  headquarters = 
+}
