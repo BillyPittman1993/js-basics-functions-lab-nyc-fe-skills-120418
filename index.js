0f9350a9db4b075cc6d4edf164cbe5bbf13f681b
BillyPittman1993 <billytrading93@gmail.com>
2019-01-18 00:51:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // Code your solution in this file!
 
 function distanceFromHqInBlocks(distance) {
-  if (distance === 43) {
+if (distance - 43 === 1) {
+return true 
+}
+else { 
+return false 
   }
-  return 
 }
