acaca79166f355b457064f31ece6b3b8b8f6f1cc
BillyPittman1993 <billytrading93@gmail.com>
2019-01-18 01:22:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 // Code your solution in this file!
 
-function distanceFromHqInBlocks(blocks) {
-  headquarters = 42 
-if (blocks - headquarters === 1) {
-return 1 
-}
-else if (blocks - headquarters === 8) { 
-return 8 
-  } 
-  else if (headquarters - blocks === 8) {
-  return 8 
+const startingPoint = 42 
+
+function distanceFromHqInBlocks(block) {
+  return block - startingPoint
+  if (block < startingPoint) {
+    return startingPoint - block 
+  } else {
+    return block - startingPoint
   }
 }
+
